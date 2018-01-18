e553f0036c2fb0a9c503c96acc384cc0ebecc319
penglei <yupengniaosanshi@gmail.com>
2018-01-18 14:38:10 +0800

    主页面列表

--- app/src/main/java/com/example/apparchitecture/MainActivity.kt
+++ app/src/main/java/com/example/apparchitecture/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.apparchitecture
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.apparchitecture.room.RoomActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val activities = listOf(RoomActivity::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listView_main.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
+                activities.map { it.simpleName })
+        listView_main.setOnItemClickListener { _, _, position, _ ->
+            startActivity(Intent(this, activities[position]))
+        }
     }
 }
---
+++ app/src/main/java/com/example/apparchitecture/room/RoomActivity.kt
@@ -0,0 +1,8 @@
+package com.example.apparchitecture.room
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by yupenglei on 18/1/18.
+ */
+class RoomActivity : AppCompatActivity()
